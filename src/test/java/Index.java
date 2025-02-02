6c8525a7c95dc9e4912ac134273816c10506a235
DariaMalykh <159196750+DariaMalykh@users.noreply.github.com>
2025-02-02 14:06:52 +0200

    CW2

--- src/test/java/Index.java
+++ src/test/java/Index.java
@@ -16,12 +16,12 @@ public class Index {
         wd.get("file:///C:/Users/u6o00/Downloads/21.index.html");
     }
     @Test
-    public void cssLocators(){
+    public void cssLocators() {
         //by tag name
-       WebElement button =  wd.findElement(By.tagName("button"));
-       WebElement button1 = wd.findElement(By.cssSelector("button"));
+        WebElement button = wd.findElement(By.tagName("button"));
+        WebElement button1 = wd.findElement(By.cssSelector("button"));
 
-       WebElement a = wd.findElement(By.tagName("a"));
+        WebElement a = wd.findElement(By.tagName("a"));
         WebElement a1 = wd.findElement(By.cssSelector("a"));
 
         List<WebElement> listA = wd.findElements(By.tagName("a"));
@@ -30,5 +30,42 @@ public class Index {
         WebElement form = wd.findElement(By.tagName("form"));
         List<WebElement> form1 = wd.findElements(By.cssSelector("form"));
 
+        //by class
+        WebElement div = wd.findElement(By.className("container"));
+        WebElement div1 = wd.findElement(By.cssSelector(".container"));
+
+        List<WebElement> a_list = wd.findElements(By.className("nav-item"));
+        List<WebElement> a_list1 = wd.findElements(By.cssSelector(".nav-item"));
+
+        //by id
+        WebElement nav = wd.findElement(By.id("nav"));
+        WebElement nav1 = wd.findElement(By.cssSelector("#nav"));
+
+        WebElement form2 = wd.findElement(By.id("form1"));
+        WebElement form3 = wd.findElement(By.cssSelector("#form1"));
+
+        //by attribute
+        WebElement inputEmail = wd.findElement(By.cssSelector("[placeholder = 'Type your name']"));
+        //start
+        WebElement inputEmail1 = wd.findElement(By.cssSelector("[placeholder ^= 'Type']"));
+        //end
+        WebElement inputEmail2 = wd.findElement(By.cssSelector("[placeholder $= 'name']"));
+        //contains
+        WebElement inputEmail3 = wd.findElement(By.cssSelector("[placeholder *= 'your']"));
+
+        WebElement a2 = wd.findElement(By.cssSelector("[href = '#item3']"));
+
+        //by name не используется
+        WebElement inputS = wd.findElement(By.name("surname"));
+        WebElement inputS1 = wd.findElement(By.cssSelector("[name = 'surname']"));
+
+        //by linkText не используется
+        WebElement a3 = wd.findElement(By.linkText("Item 1"));
+        //partialLinkText
+        WebElement a4 = wd.findElement(By.partialLinkText("m 1"));
+
+
     }
-}
+
+    }
+
